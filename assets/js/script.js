4dbaa66716de3a6e34bb572974ef12f1934bdade
Dan B <dbraunby@hotmail.com>
2021-02-09 11:47:38 -0700

    get info from form

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,22 @@ let createTaskHandler = function() {
     //prevent auto refresh on submit
     event.preventDefault();
 
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //Create new line item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //Create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    //Add html to div
+    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
